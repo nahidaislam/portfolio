b90170101eb08b4d8ad766f8a747651f8f694ff8
Nahida Islam <nahida@kth.se>
2020-06-28 15:53:07 +0200

    Updates

---
+++ precache-manifest.38a35a5109f0560820770816874c5767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6d36fe46053180def12f5798424e9b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9d38658e925f3c1b7993",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/Portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d38658e925f3c1b7993",
+    "url": "/Portfolio/static/js/main.0e65fed2.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
