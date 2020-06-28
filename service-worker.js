305e59e2cd4f424e850f9c7c8fc63cf26a46fe54
Nahida Islam <nahida@kth.se>
2020-06-28 16:15:39 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.38a35a5109f0560820770816874c5767.js"
+  "/Portfolio/precache-manifest.c3e2125a68e85909e7721ee42a73524b.js"
 );
 
 self.addEventListener('message', (event) => {
